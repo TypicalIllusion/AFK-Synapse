e94e1c226622eceb8ac0d7466c6e3653af5c472d
TypicalIllusion <70919038+TypicalIllusion@users.noreply.github.com>
2020-12-22 12:14:18 -0500

    Update Plugin.cs

--- AFK(Synapse)/Plugin.cs
+++ AFK(Synapse)/Plugin.cs
@@ -10,7 +10,7 @@ namespace AFK_Synapse_
        SynapseMajor = 2,
        SynapseMinor = 3,
        SynapsePatch = 0,
-       Version = "1.0.0"
+       Version = "1.0.1"
    )]
     class Plugin : AbstractPlugin
     {
